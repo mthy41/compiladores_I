82463b61889f401b3aee033bfb6520308d46c623
renegadelhaedu <renegadelha.edu@gmail.com>
2025-03-15 14:55:01 -0300

    first commit

---
+++ Programa.lex
@@ -0,0 +1,9 @@
+int main(){
+
+    int x = 2;
+
+    if(x > 4){
+        printf("d", x);
+    }
+
+}
---
+++ src/com/Auxiliares/TESTA.java
@@ -0,0 +1,9 @@
+package com.Auxiliares;
+
+public class TESTA {
+    public static void main(String[] args) {
+        int x = 0;
+        ++x;
+        System.out.println(x);
+    }
+}
